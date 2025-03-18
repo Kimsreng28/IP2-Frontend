8de7b852bfe924112f744bb369b04b5b23c5d4e8
CHAN Suvannet <chansuvannet999@gmail.com>
2025-03-18 10:11:27 +0700

    modify file structure

---
+++ src/app/(site)/client/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+export default function Client() {
+  return (
+    <>
+      <div className="flex justify-center items-center h-screen">
+        <h1>Client Page </h1>
+      </div>
+    </>
+  );
+}
---
+++ src/app/(site)/vendor/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+export default function Vendor() {
+  return (
+    <>
+      <div className="flex justify-center items-center h-screen">
+        <h1>Vendor Page </h1>
+      </div>
+    </>
+  );
+}
