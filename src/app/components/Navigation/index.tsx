1551b63fa70a433da9ccc5b55c31b09adde64a63
Kimsreng28 <kimsrenghai@gmail.com>
2025-04-08 20:18:27 +0700

    Authentication with google

--- src/app/components/Navigation/index.tsx
+++ src/app/components/Navigation/index.tsx
@@ -76,10 +76,10 @@ const Navigation = () => {
 
   return (
     <header
-      className={`fixed left-0 top-0 z-99999 w-full py-7 ${stickyMenu ? "bg-white !py-2 shadow transition duration-100 dark:bg-black" : ""}`}
+      className={`fixed left-0 top-0 z-99999 w-full py-7 font-poppins ${stickyMenu ? "bg-white !py-2 shadow transition duration-100 dark:bg-black" : ""}`}
     >
-      <div className="relative mx-auto max-w-c-1390 items-center justify-between px-4 md:px-8 xl:flex 2xl:px-0">
-        <div className="flex w-full items-center justify-between xl:w-1/4">
+      <div className="relative mx-auto max-w-c-1390 items-center justify-between px-4 font-poppins md:px-8 xl:flex 2xl:px-0">
+        <div className="flex w-full items-center justify-between font-poppins xl:w-1/4">
           <a href="/" className="text-xl">
             Els-Sale
           </a>
@@ -112,10 +112,10 @@ const Navigation = () => {
 
         {/* Navigation Menu */}
         <div
-          className={`invisible h-0 w-full items-center justify-center xl:visible xl:flex xl:h-auto xl:w-full ${navigationOpen && "navbar !visible mt-4 h-auto max-h-[400px] rounded-md bg-white p-7.5 shadow-solid-5 dark:bg-blacksection xl:h-auto xl:p-0 xl:shadow-none xl:dark:bg-transparent"}`}
+          className={` invisible h-0 w-full items-center justify-center xl:visible xl:flex xl:h-auto xl:w-full ${navigationOpen && "navbar !visible mt-4 h-auto max-h-[400px] rounded-md bg-white p-7.5 shadow-solid-5 dark:bg-blacksection xl:h-auto xl:p-0 xl:shadow-none xl:dark:bg-transparent"}`}
         >
           <nav>
-            <ul className="mr-[300px] flex flex-col gap-5 xl:flex-row xl:items-center xl:gap-10">
+            <ul className="mr-[300px] flex flex-col gap-5 font-poppins xl:flex-row xl:items-center xl:gap-10">
               {menuData.map((menuItem, key) => (
                 <li key={key} className={"group relative"}>
                   <Link
@@ -123,7 +123,7 @@ const Navigation = () => {
                     onClick={() => updatePath(menuItem.path)} // Update path on click and close menu
                     className={
                       currentPath === menuItem.path
-                        ? "rounded-full   py-2 font-semibold dark:bg-[#343c5c]"
+                        ? "rounded-full py-2 font-semibold dark:bg-[#343c5c]"
                         : ""
                     }
                   >
