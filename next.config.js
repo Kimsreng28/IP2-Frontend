5b2abf3c5467a87df5e415ad173df05b0f867827
CHAN Suvannet <chansuvannet999@gmail.com>
2025-04-11 12:25:52 +0700

    setup env

--- next.config.js
+++ next.config.js
@@ -1,17 +1,23 @@
-/** @type {import('next').NextConfig} */
+// next.config.js
 
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ["localhost"],
+    domains: ['localhost'],
     remotePatterns: [
       {
-        protocol: "https",
-        hostname: "cdn.sanity.io",
-        port: "",
+        protocol: 'https',
+        hostname: 'cdn.sanity.io',
+        port: '',
       },
     ],
+  },
+  env: {
+    API_BASE_URL: process.env.API_BASE_URL,
+    FILE_BASE_URL: process.env.FILE_BASE_URL,
+    RecaptchaSiteKey: process.env.RecaptchaSiteKey,
   },
   async redirects() {
     return [
--- src/envs/env.ts
+++ src/envs/env.ts
@@ -1,4 +1,8 @@
-export const env = {
+
+const env = {
     production: true,
-    API_BASE_URL      : process.env.API_BASE_URL,
+    API_BASE_URL: process.env.API_BASE_URL,
+    FILE_BASE_URL: process.env.FILE_BASE_URL,
+    RecaptchaSiteKey: process.env.RecaptchaSiteKey
 };
+export default env; 
