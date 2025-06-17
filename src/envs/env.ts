c36ed5926a3e5f779428408cefe357b655e08711
CHAN Suvannet <chansuvannet999@gmail.com>
2025-06-17 20:21:57 +0700

    fix docker file

--- src/envs/env.ts
+++ src/envs/env.ts
@@ -3,6 +3,7 @@ const env = {
     production: true,
     API_BASE_URL: process.env.API_BASE_URL,
     FILE_BASE_URL: process.env.FILE_BASE_URL,
+    RESEND_API_KEY: process.env.FILE_BASE_URL,
     RecaptchaSiteKey: process.env.RecaptchaSiteKey
 };
 export default env; 
